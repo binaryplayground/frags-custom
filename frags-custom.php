216044f32c4e35b29a483c7d02ceb032e55b6c18
findingsimple <jason@findingsimple.com>
2025-07-18 04:24:49 +0000

    Add plugin file

---
+++ frags-custom.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Frags.au custom code
+ * Plugin URI:        https://frags.au/
+ * Description:       Custom code.
+ * Version:           0.1
+ * Requires at least: 6.0
+ * Requires PHP:      8.0
+ * Author:            findingsimple
+ * Author URI:        https://findingsimple.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/binaryplayground/frags-custom/
+ * Text Domain:       frags-custom
+ */
+
